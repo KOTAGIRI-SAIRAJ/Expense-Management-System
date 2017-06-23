c81ca2f038d4a28716c1da300b1336aacb3d6299
semanticbits <sairaj.kotagiri@india.semanticbits.com>
2017-06-23 20:29:11 +0530

    Intial commit

---
+++ Software/restapi/server/migrations/create-expense.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const expenseStatus = require("../enums/expenseStatus")
+const expenseType = require("../enums/expenseType")
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('expenses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      amount: {
+        type: Sequelize.INTEGER
+      },
+      expenseDate: {
+        type: Sequelize.DATE
+      },
+      expensetype: {
+        type: Sequelize.ENUM,
+        values:expenseStatus.values
+      },
+      projectId: {
+        type: Sequelize.INTEGER
+      },
+      resourceId: {
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values:expenseStatus.values
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('expenses');
+  }
+};
---
+++ Software/restapi/server/models/expense.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const expenseStatus = require("../enums/expenseStatus")
+const expenseType = require("../enums/expenseType")
+
+module.exports = function(sequelize, DataTypes) {
+  var expense = sequelize.define('expense', {
+    title: DataTypes.STRING,
+    amount: DataTypes.INTEGER,
+    expenseDate: DataTypes.DATE,
+    expensetype:{
+      type:DataTypes.ENUM,
+      values:expenseStatus.values
+    },
+    projectId: DataTypes.INTEGER,
+    resourceId: DataTypes.INTEGER,
+    status:{
+      type:DataTypes.ENUM,
+      values:expenseStatus.values
+    },
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return expense;
+};
