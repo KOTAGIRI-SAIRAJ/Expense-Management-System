3f32ec934148aee26f723db7214324bdbb05028f
semanticbits <sairaj.kotagiri@india.semanticbits.com>
2017-06-27 20:31:33 +0530

    Intial commit

--- Software/client/ExpenseManagemanetSystem/src/app/app.service.ts
+++ Software/client/ExpenseManagemanetSystem/src/app/app.service.ts
@@ -3,12 +3,10 @@
  */
 export class localStorageService{
   setLocalStorageValue(value){
-    console.log('from LocalStorge set')
     console.log(value);
     localStorage.setItem('LoggedIn User',JSON.stringify(value));
   }
   getLocalStorageValue(){
-    console.log('from LocalStorge get')
     return JSON.parse(localStorage.getItem('LoggedIn User'));
   }
 }
