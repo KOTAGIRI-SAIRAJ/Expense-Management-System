10f927c7f86342b0ac37f4b3487c09104c99d6fa
semanticbits <sairaj.kotagiri@india.semanticbits.com>
2017-07-07 13:25:52 +0530

    feature

--- Software/restapi/server/api/project_resource/dao/project_resource-dao.js
+++ Software/restapi/server/api/project_resource/dao/project_resource-dao.js
@@ -8,7 +8,9 @@ export default class resourceDAO{
   static createNew(request,res) {
     return new Promise((resolve, reject) => {
       models.project.findById(request.projectId).then(projec=>{
+
         models.resources.findById(request.resourceId).then(reso=>{
+
           projec.setResources([reso]).then(()=>{})
         })
       })
